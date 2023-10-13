af508beb2513dceb8c21ac7d4e73d46a1cd75039
AbhiramiKarthikeyan <abhiramikarthikeyan29@gmail.com>
2023-10-13 10:37:07 +0530

    amazone-clone

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-tp4dxq3mz26ckbm0.us.auth0.com"
+      clientId="K7E9h1CRLi10dhuhM6fDmedtXHjUUOuk"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
